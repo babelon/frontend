dde790993b785034dcb8d60d0a960f43875a8f2e
Zahan Malkani <zahanm@gmail.com>
2012-04-05 12:15:05 -0700

    javascript to display email and fade in

---
+++ static/scripts/index.js
@@ -0,0 +1,52 @@
+(function() {
+
+  /*
+   * show = boolean of whether it should be diplayed in the end or not
+   */
+  function fadeTo (el, targetopacity, callback) {
+    var stepsize, steplength;
+
+    // stupidity checks
+    if (targetopacity === 1) el.style.opacity = 0;
+    else if (targetopacity === 0) el.style.opacity = 1;
+
+    stepsize = 0.08;
+    steplength = 10;
+
+    function step() {
+      if (el.style.opacity === targetopacity.toString()) {
+        callback();
+        return;
+      }
+
+      if (el.style.opacity < targetopacity) {
+        el.style.opacity = parseFloat(el.style.opacity) + stepsize;
+        if (el.style.opacity > targetopacity) el.style.opacity = targetopacity;
+      } else {
+        el.style.opacity = parseFloat(el.style.opacity) - stepsize;
+        if (el.style.opacity < targetopacity) el.style.opacity = targetopacity;
+      }
+
+      setTimeout(step, steplength);
+    }
+
+    step();
+  }
+
+  window.document.addEventListener('DOMContentLoaded', function() {
+    var btn;
+
+    btn = document.getElementById('btn_learnmore');
+    btn.addEventListener('click', function(ev) {
+      var learnmore;
+
+      learnmore = document.getElementById('learnmore');
+      learnmore.style.opacity = 0;
+      learnmore.style.display = 'block';
+      fadeTo(learnmore, 1, function() {
+        document.getElementById('email').focus();
+      });
+    });
+  });
+
+})();
